13f561e47458ec874363566bb44e5a2417980ad7
Hideki Abe <hideki.abe.0630@gmail.com>
2017-10-28 11:50:22 +0900

    First commit
    Plugin base generated on pluginfactory.io

---
+++ src/translations/en/heading-level-slider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * HeadingLevelSlider plugin for Craft CMS 3.x
+ *
+ * Slide level of HTML hx(h1-h6) tag 
+ *
+ * @link      https://www.anothersky.pw
+ * @copyright Copyright (c) 2017 Hideki Abe
+ */
+
+/**
+ * HeadingLevelSlider en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('heading-level-slider', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Hideki Abe
+ * @package   HeadingLevelSlider
+ * @since     1.0.0
+ */
+return [
+    'HeadingLevelSlider plugin loaded' => 'HeadingLevelSlider plugin loaded',
+];
---
+++ src/twigextensions/HeadingLevelSliderTwigExtension.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * HeadingLevelSlider plugin for Craft CMS 3.x
+ *
+ * Slide level of HTML hx(h1-h6) tag 
+ *
+ * @link      https://www.anothersky.pw
+ * @copyright Copyright (c) 2017 Hideki Abe
+ */
+
+namespace hidekia\headinglevelslider\twigextensions;
+
+use hidekia\headinglevelslider\HeadingLevelSlider;
+
+use Craft;
+
+/**
+ * Twig can be extended in many ways; you can add extra tags, filters, tests, operators,
+ * global variables, and functions. You can even extend the parser itself with
+ * node visitors.
+ *
+ * http://twig.sensiolabs.org/doc/advanced.html
+ *
+ * @author    Hideki Abe
+ * @package   HeadingLevelSlider
+ * @since     1.0.0
+ */
+class HeadingLevelSliderTwigExtension extends \Twig_Extension
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'HeadingLevelSlider';
+    }
+
+    /**
+     * Returns an array of Twig filters, used in Twig templates via:
+     *
+     *      {{ 'something' | someFilter }}
+     *
+     * @return array
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('hnSlider', [$this, 'hnSlider'], array(
+            'is_safe' => array('html'))),
+        ];
+    }
+
+    /**
+     * Returns an array of Twig functions, used in Twig templates via:
+     *
+     *      {% set this = someFunction('something') %}
+     *
+    * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('hnSlider', [$this, 'hnSlider']),
+        ];
+    }
+
+    /**
+     * 
+     *
+     * @param null $text
+     *
+     * @return string
+     */
+    /**
+     * @return string
+     */
+    public function hnSlider($text = null)
+    {
+        $meta = '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">';
+        $result = \Vikpe\HtmlHeadingNormalizer::min($meta . $text, 3);
+        $result = str_replace($meta, '', $result);
+
+        return $result;
+    }
+}
